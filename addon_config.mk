f3da9fc53078ba5390b3c9c68fd6250d68b75f04
Dylan Marcus <dylan@mach1studios.com>
2021-03-11 17:31:27 -0500

    added to addon config

--- addon_config.mk
+++ addon_config.mk
@@ -6,13 +6,18 @@ meta:
 	ADDON_URL = "https://github.com/Mach1Studios/ofxMetaMotion"
 
 common:
-
-osx:
-
-vs:
-
-linux64:
-
 linux:
-
+linux64:
+linuxarmv6l:
 linuxarmv7l:
+android/armeabi-v7a:
+android/x86:
+osx:
+	ADDON_LIBS += libs/libmetawear/lib/osx/libmetawear.a
+	ADDON_LIBS += libs/libnativeble/lib/osx/libnativeble-static.a
+ios:
+tvos:
+msys2:
+vs:
+	ADDON_LIBS += libs/libmetawear/lib/vs/x64/Release/MetaWear.Win32.lib
+	ADDON_LIBS += libs/libnativeble/lib/vs/x64/Release/nativeble-static.lib
